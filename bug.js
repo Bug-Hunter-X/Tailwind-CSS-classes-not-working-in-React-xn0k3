106fbdd7fbf4f1255bb9192f322d0cca0cb6017e
BugHunter-X <askzenai@gmail.com>
2025-01-29 23:28:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,13 @@
+```javascript
+//In your main app.js file
+import 'tailwindcss/tailwind.css';
+
+// ...rest of your code
+```
+
+```javascript
+//In one of your component's js file
+<div className="bg-red-500">
+{/*This class is not working*/}
+</div>
+```
